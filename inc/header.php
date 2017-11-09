e3705179a6d3972875a3e86529b04747cfbb72e9
TaniaScorte <tania.scortegagna@gmail.com>
2017-11-09 19:45:50 -0300

    Cambios en header y footer

--- inc/header.php
+++ inc/header.php
@@ -1,30 +1,25 @@
 <?php
     session_start();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<body >
-    <div class="container">
-        <header>
-            <nav class="navbar navbar-default navbar-fixed-top">
-                <div class="col-xs-4">
-                    <h3 "navbar-text">Francoise Dolto</h3>
-                </div>
-                   <div class="dropdown pull-right">
-                       <button class="btn btn-barra navbar-btn dropdown-toggle glyphicon glyphicon-user" type="button" data-toggle="dropdown"> 
-                       <?php
-                            echo "Hola!,".$_SESSION['usuario'];
-                        ?>
-                        </button>
-                       <ul class="dropdown-menu">
-                          <li><a href="../inc/Logout.php">Salir</a></li>
-                      </ul>
-                   </div> 
-                <div class="pull-right">
-                    <button class="btn btn-barra navbar-btn "> Reporte de asistencia</button>
-                </div>           
-             </nav>         
-        </header>
-</body>
-</html>
+<header>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="col-xs-4">
+            <h3 "navbar-text">Francoise Dolto</h3>
+        </div>
+            <div class="dropdown pull-right">
+                <button class="btn btn-barra navbar-btn dropdown-toggle glyphicon glyphicon-user" type="button" data-toggle="dropdown"> 
+                <?php
+                    echo "Hola!,".$_SESSION['usuario'];
+                ?>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a href="../inc/Logout.php">Salir</a></li>
+                </ul>
+            </div> 
+        <div class="pull-right">
+            <button class="btn btn-barra navbar-btn "> Reporte de asistencia</button>
+        </div>           
+        </nav>         
+</header>
+
 
