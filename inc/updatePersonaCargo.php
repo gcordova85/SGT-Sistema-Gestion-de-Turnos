cdc8d72e3064b7da9ffa996a58cd92c8cf7eb305
juan <juann.c.n@gmail.com>
2017-11-29 06:32:13 -0300

    Mejoras en validaciones

--- inc/updatePersonaCargo.php
+++ inc/updatePersonaCargo.php
@@ -12,26 +12,58 @@ $statement = $cnn->prepare($sql);
 
 $respuesta=false;
 
-//echo $personas;
-//var_dump($personas->{"data"}); //asi accedo al array var_dump es para enviar al cliente y mediante js mostrar en consola
-foreach($personas->{"data"} as $persona){
-    //echo $persona->{"id_personaCargo"};
-    
-    //echo $persona->{"nombre"};
-    //echo $producto->{"precio"};
-    
-    $statement->bindParam(":id", $persona->{"id_personaCargo"},PDO::PARAM_INT); 
-    $statement->bindParam(":nombre", $persona->{"nombre"},PDO::PARAM_STR); 
-    $statement->bindParam(":apellido", $persona->{"apellido"},PDO::PARAM_STR);
-    $statement->bindParam(":dni", $persona->{"dni"},PDO::PARAM_INT);
-    $statement->bindParam(":direccion", $persona->{"direccion"},PDO::PARAM_STR);    
-    $statement->bindParam(":telefono", $persona->{"telefono"},PDO::PARAM_INT);
-    
-    $respuesta=$statement->execute();
+$enviar=true;
 
+foreach($personas->{"data"} as $persona){
+    $pattern='/([A-Za-zñáéíóú]{3,})\s*(([A-Za-zñáéíóú]{3,})){0,1}$/';   
+    $success = preg_match($pattern, $persona->{"nombre"});
+    if (!$success) {
+        echo "Formato de nombre incorrecto";
+        
+        $enviar=false;
+        }
+    $success = preg_match($pattern, $persona->{"apellido"});
+    if (!$success) {
+        echo "Formato de apellido incorrecto";      
+        $enviar=false;
+    }
+    if(!filter_var($persona->{"dni"}, FILTER_VALIDATE_INT) === 0 || filter_var($persona->{"apellido"}, FILTER_VALIDATE_INT) === true)
+    {
+        echo "Ingresa solo numeros en dni";
+        $enviar=false;
+    }
+    if(strlen($persona->{"direccion"})<5){
+        echo "La direccion no es valida";
+        $enviar=false;
     }
 
-echo $respuesta;
+    if(!filter_var($persona->{"telefono"}, FILTER_VALIDATE_INT) === 0 || filter_var($persona->{"apellido"}, FILTER_VALIDATE_INT) === true)
+    {
+        echo "Ingresa solo numeros en telefono";
+        $enviar=false;
+    }
+}
+
+    if ($enviar){
+
+        foreach($personas->{"data"} as $persona){     
+            
+            $statement->bindParam(":id", $persona->{"id_personaCargo"},PDO::PARAM_INT); 
+            $statement->bindParam(":nombre", $persona->{"nombre"},PDO::PARAM_STR); 
+            $statement->bindParam(":apellido", $persona->{"apellido"},PDO::PARAM_STR);
+            $statement->bindParam(":dni", $persona->{"dni"},PDO::PARAM_INT);
+            $statement->bindParam(":direccion", $persona->{"direccion"},PDO::PARAM_STR);    
+            $statement->bindParam(":telefono", $persona->{"telefono"},PDO::PARAM_INT);
+            
+            $respuesta=$statement->execute();
+        
+            }    
+
+        echo $respuesta;        
+    }else{
+        return "Error";
+    }
+
 
 
 
